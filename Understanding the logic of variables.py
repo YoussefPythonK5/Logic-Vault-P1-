a8513405b1ab4125e3a778a08856df4ce1ea69bd
Youssef <ya103857@gmail.com>
2026-03-20 04:26:15 +0200

    Add files via upload

---
+++ Understanding the logic of variables.py
@@ -0,0 +1,22 @@
+
+#The correct way to write code
+
+name = "YoussefــDeveloper"
+
+print (name)
+
+car = "BMWــM3ــGTR"
+
+print (car)
+
+print("***************************")
+
+#Example of an incorrect way to write code
+
+name = "YoussefــDeveloper"
+
+print ("name"")
+
+car = "BMWــM3ــGTR"
+
+print ("car")
